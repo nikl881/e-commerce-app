13ad5a4eb2afe93a06c92868758d5f21cc39d1dc
Niels <deklerkn@xs4all.nl>
2019-05-10 18:51:47 +0000

    if else db for travis

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -78,6 +78,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 if "DATABASE_URL" in os.environ:
     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
@@ -88,7 +90,7 @@ else:
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
-}
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
